c8883417885f81477dd4ce0642ad3bb95dc5f65d
dbarabander <barabander@gmail.com>
2015-04-15 17:30:59 -0400

    advanced solution complete

---
+++ ScrollViewAutoLayout/ScrollViewAutoLayout/CustomTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.h
+//  ScrollViewAutoLayout
+//
+//  Created by Daniel Barabander on 4/15/15.
+//  Copyright (c) 2015 Flatiron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell
+
+@end
---
+++ ScrollViewAutoLayout/ScrollViewAutoLayout/CustomTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.m
+//  ScrollViewAutoLayout
+//
+//  Created by Daniel Barabander on 4/15/15.
+//  Copyright (c) 2015 Flatiron. All rights reserved.
+//
+
+#import "CustomTableViewCell.h"
+
+@implementation CustomTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
