35570673212055ee1d1ce0647b0f69ebdaf8a227
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-12 10:03:57 -0500

    Completes Extra Credit 1: Custom UITableViewCell.

---
+++ ScrollViewAutoLayout/FISTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  FISTableViewCell.h
+//  ScrollViewAutoLayout
+//
+//  Created by Ken M. Haggerty on 2/12/16.
+//  Copyright © 2016 Flatiron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISTableViewCell : UITableViewCell
+@property (nonatomic, strong) IBOutlet UIImageView *imageView1;
+@property (nonatomic, strong) IBOutlet UIImageView *imageView2;
+@property (nonatomic, strong) IBOutlet UIImageView *imageView3;
+@property (nonatomic, strong) IBOutlet UIImageView *imageView4;
+@property (nonatomic, strong) IBOutlet UIImageView *imageView5;
+@end
