6ae350be965287aa48ac5b54a895fcf07da27a38
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-12 10:16:47 -0500

    Completes Extra Credit 2: UIScrollView with keyboard.

---
+++ ScrollViewAutoLayout/FISScrollViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISScrollViewController.h
+//  ScrollViewAutoLayout
+//
+//  Created by Ken M. Haggerty on 2/12/16.
+//  Copyright © 2016 Flatiron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISScrollViewController : UIViewController
+
+@end
---
+++ ScrollViewAutoLayout/FISScrollViewController.m
@@ -0,0 +1,37 @@
+//
+//  FISScrollViewController.m
+//  ScrollViewAutoLayout
+//
+//  Created by Ken M. Haggerty on 2/12/16.
+//  Copyright © 2016 Flatiron. All rights reserved.
+//
+
+#import "FISScrollViewController.h"
+
+@interface FISScrollViewController ()
+
+@end
+
+@implementation FISScrollViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
